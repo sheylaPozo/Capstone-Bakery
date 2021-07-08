b9c77cbd77576990d0a6a609ee07ab89ab751690
sheylaPozo <shiggipoke@gmail.com>
2021-07-08 12:46:10 -0500

    add mobile version, movile cards, javascript

---
+++ mobile-menu.js
@@ -0,0 +1,88 @@
+const body = document.getElementById('html');
+const menu = document.createElement('div');
+menu.className = 'mobile-menu-style';
+body.appendChild(menu);
+
+const icon = document.createElement('i');
+icon.className = 'fas fa-times fa-2x';
+icon.id = 'close';
+menu.appendChild(icon);
+
+const menuList = document.createElement('ul');
+menuList.className = 'menu-list';
+menuList.id = 'mobileMenuLinks';
+menu.appendChild(menuList);
+const home = document.createElement('li');
+const homeLink = document.createElement('a');
+homeLink.className = 'blur-effect';
+homeLink.textContent = 'Home';
+homeLink.href = 'index.html';
+home.appendChild(homeLink);
+menuList.appendChild(home);
+
+const about = document.createElement('li');
+const aboutLink = document.createElement('a');
+aboutLink.className = 'blur-effect';
+aboutLink.textContent = 'About';
+aboutLink.href = 'about.html';
+about.appendChild(aboutLink);
+menuList.appendChild(about);
+
+const program = document.createElement('li');
+const programLink = document.createElement('a');
+programLink.className = 'blur-effect';
+programLink.textContent = 'Program';
+programLink.href = '#';
+program.appendChild(programLink);
+menuList.appendChild(program);
+
+const sponsor = document.createElement('li');
+const sponsorLink = document.createElement('a');
+sponsorLink.className = 'blur-effect';
+sponsorLink.textContent = 'Bestsellers';
+sponsorLink.href = '#';
+sponsor.appendChild(bestsellersLink);
+menuList.appendChild(bestsellers);
+
+const news = document.createElement('li');
+const newsLink = document.createElement('a');
+newsLink.className = 'blur-effect';
+newsLink.textContent = 'News';
+newsLink.href = '#';
+news.appendChild(newsLink);
+menuList.appendChild(news);
+
+const join = document.createElement('li');
+const joinLink = document.createElement('a');
+joinLink.className = 'blur-effect';
+joinLink.textContent = 'Join';
+joinLink.href = '#';
+join.appendChild(joinLink);
+menuList.appendChild(join);
+
+const socialMedia = document.createElement('li');
+const socialMediaLink = document.createElement('a');
+socialMediaLink.className = 'mobile-menu-social-media';
+socialMediaLink.href = '#';
+socialMedia.appendChild(socialMediaLink);
+menuList.appendChild(socialMedia);
+
+const socialMediaFacebook = document.createElement('i');
+socialMediaFacebook.className = 'fab fa-facebook-f';
+socialMediaFacebook.id = 'mobileMenuFacebook';
+socialMediaLink.appendChild(socialMediaFacebook);
+
+const socialMediaTwitter = document.createElement('i');
+socialMediaTwitter.className = 'fab fa-twitter';
+socialMediaTwitter.id = 'mobileMenuTwitter';
+socialMediaLink.appendChild(socialMediaTwitter);
+
+document.getElementById('hamburger').addEventListener('click', () => {
+    menu.style.display = 'block';
+    document.getElementById('body').style.filter = 'blur(5px)';
+});
+
+icon.addEventListener('click', () => {
+    menu.style.display = 'none';
+    document.getElementById('body').style.filter = 'none';
+});
---
+++ script.js
@@ -0,0 +1,192 @@
+const main = document.getElementById('main');
+const Latestadditions = document.createElement('section');
+Latestadditions.className = 'main-section-desserts';
+Latestadditions.id = 'Latestadditions';
+main.appendChild(Latestadditions);
+
+const featuredDancersDIV = document.createElement('div');
+LatestadditionsDIV.className = 'main-section-desserts-content';
+Latestadditions.appendChild(LatestadditionsDIV);
+
+const LatestadditionsTitle = document.createElement('h3');
+LatestadditionsTitle.className = 'main-section-desserts-title';
+featuredDessertsTitle.textContent = 'Featured Desserts';
+featuredDessertsDIV.appendChild(featuredDessertsTitle);
+
+const featuredDessertsTitleUnderline = document.createElement('p');
+featuredDessertsTitleUnderline.className = 'footer-top-underline display-flex-center';
+featuredDessertsDIV.appendChild(featuredDessertsTitleUnderline);
+
+const featuredDessertsTitleUnderlineSp = document.createElement('span');
+featuredDessertsTitleUnderlineSp.className = 'footer-top-underline-span';
+featuredDessertsTitleUnderlineSp.className = 'footer-top-underline-span';
+featuredDessertsTitleUnderline.appendChild(featuredDessertsTitleUnderlineSp);
+
+const cardUL = document.createElement('ul');
+cardUL.className = 'main-section-dancers-cards';
+cardUL.id = 'cards-show';
+featuredDessertsDIV.appendChild(cardUL);
+
+function ProjectCard(imageLink, imageAltText, title, descriprion, textContent) {
+    this.image = {
+        link: imageLink,
+        altText: imageAltText,
+    };
+    this.title = title;
+    this.descriprion = descriprion;
+    this.textContent = textContent;
+}
+
+const desserts = [];
+
+desserts.push(
+    new ProjectCard(
+        'images/.',
+        'Picture of Dessert',
+        'Pudin cake',
+        "Vanilla and chocolate pudin cookie's cake - special recipe with cookies'",
+        'In 2001 Yve´s Konditorei won a first place in World Baking Compettitioon for the best bakeries on the Country, so is recognoized Country level for its merits',
+    ),
+);
+
+desserts.push(
+    new ProjectCard(
+        'images/',
+        'Picture of Dessert',
+        'Tiramissu',
+        "Made with the secret recipe of grandma Paula, tradition of the house'",
+        'In 2001 Yve´s Konditorei won a first place in World Baking Compettitioon for the best bakeries on the Country, so is recognoized Country level for its merits',
+    ),
+);
+
+desserts.push(
+    new ProjectCard(
+        'images/',
+        'Picture of Dessert',
+        'Panacota',
+        "Italian dessert, made with the best German ingredients to make its taste dedlicious like made in heaven",
+        'In 2001 Yve´s Konditorei won a first place in World Baking Compettitioon for the best bakeries on the Country, so is recognoized Country level for its merits.',
+    ),
+);
+
+desserts.push(
+    new ProjectCard(
+        'images/',
+        'Picture of Dessert',
+        'Personalized Cupcakes',
+        "We always come with different ideas for our cupcakes, sometimes rainbow, other days galaxy, next day bicolor, it's always matter of creativity and creation",
+        'In 2001 Yve´s Konditorei won a first place in World Baking Compettitioon for the best bakeries on the Country, so is recognoized Country level for its merits',
+    ),
+);
+
+desserts.push(
+    new ProjectCard(
+        'images/',
+        'Picture of Dessert',
+        'Birthday, Wedding, celebration Cakes',
+        "Our cakes are of the best selection in all the flavors possible, and delicious with our secret recipes",
+        'In 2001 Yve´s Konditorei won a first place in World Baking Compettitioon for the best bakeries on the Country, so is recognoized Country level for its merits.',
+    ),
+);
+
+desserts.push(
+    new ProjectCard(
+        'images/',
+        'Picture of Dessert',
+        'Pralinem',
+        "Our chocolates are of the best selection with delicious fillers and sold in dozen or more for parties or events'",
+        'In 2001 Yve´s Konditorei won a first place in World Baking Compettitioon for the best bakeries on the Country, so is recognoized Country level for its merits.',
+    ),
+);
+
+for (let i = 0; i < desserts.length; i += 1) {
+    const cardItem = document.createElement('li');
+    cardItem.className = 'main-section-desserts-card';
+    cardUL.appendChild(cardItem);
+
+    const blackAndWhite = document.createElement('img');
+    blackAndWhite.className = 'black-and-white';
+    blackAndWhite.src = 'images/black-white-squares.jpg';
+    blackAndWhite.altText = 'Background image - black-white-squares';
+    cardItem.appendChild(blackAndWhite);
+
+    const cardImg = document.createElement('img');
+    cardImg.className = 'main-section-desserts-card-image';
+    cardImg.src = desserts[i].image.link;
+    cardImg.altText = desserts[i].image.altText;
+    cardItem.appendChild(cardImg);
+
+    const cardContent = document.createElement('div');
+    cardContent.className = 'main-section-desserts-card-content';
+    cardItem.appendChild(cardContent);
+
+    const cardTitle = document.createElement('h3');
+    cardTitle.className = 'main-section-desserts-card-title';
+    cardTitle.textContent = desserts[i].title;
+    cardContent.appendChild(cardTitle);
+
+    const cardDecription = document.createElement('p');
+    cardDecription.className = 'main-section-desserts-card-description';
+    cardDecription.textContent = desserts[i].descriprion;
+    cardContent.appendChild(cardDecription);
+
+    const cardText = document.createElement('p');
+    cardText.className = 'main-section-desserts-card-text';
+    cardText.textContent = desserts[i].textContent;
+    cardContent.appendChild(cardText);
+}
+
+function checkWindowWidth() {
+    for (let j = 0; j < 6; j += 1) {
+        if (window.screen.width < 768 && j > 1) {
+            document.getElementsByClassName('main-section-desserts-card')[
+                j
+            ].style.display = 'none';
+        } else {
+            document.getElementsByClassName('main-section-desserts-card')[
+                j
+            ].style.display = 'flex';
+        }
+    }
+}
+
+window.addEventListener('load', checkWindowWidth);
+window.addEventListener('resize', checkWindowWidth);
+
+const buttonMore = document.createElement('button');
+buttonMore.className = 'main-button-more';
+buttonMore.textContent = 'MORE';
+featuredDancersDIV.appendChild(buttonMore);
+
+const buttonIcon = document.createElement('i');
+buttonIcon.className = 'fas fa-chevron-down';
+buttonMore.appendChild(buttonIcon);
+
+let buttonStatus = false;
+
+buttonMore.addEventListener('click', () => {
+    if (buttonStatus === false) {
+        for (let j = 0; j < 6; j += 1) {
+            document.getElementsByClassName('main-section-desserts-card')[
+                j
+            ].style.display = 'flex';
+        }
+
+        buttonStatus = true;
+        buttonMore.textContent = 'LESS';
+        const buttonIcon = document.createElement('i');
+        buttonIcon.className = 'fas fa-chevron-up';
+        buttonMore.appendChild(buttonIcon);
+    } else {
+        for (let j = 2; j < 6; j += 1) {
+            document.getElementsByClassName('main-section-desserts-card')[
+                j
+            ].style.display = 'none';
+        }
+        buttonStatus = false;
+        buttonMore.textContent = 'MORE';
+        const buttonIcon = document.createElement('i');
+        buttonIcon.className = 'fas fa-chevron-down';
+        buttonMore.appendChild(buttonIcon);
+    }
+});
